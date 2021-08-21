b6c3b9f69f5a26392672a0c45a632e4a2b86c3cb
Matheus Furlan <furlan.dmtr@gmail.com>
2021-08-21 13:00:57 -0400

    montando a lista com script setup

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,57 @@
 <template>
-<input type="text" v-model="itemCompra">
-<button> Adicionar </button>
-<p>{{itemCompra}}</p>
+  <h1>{{ titulo }}</h1>
+  <input type="text" v-model="item.descricao" placeholder="Item de Compra" />
+  <app-btn @click="addItem()">Adicionar</app-btn>
+  <!-- Montando a tabela de lista de compras -->
+  {{listaCompras}}
+  <table border="1">
+    <thead>
+      <th>Carrinho</th>
+      <th>Item</th>
+      <th>Valor</th>
+    </thead>
+    <tr v-for="iterador in listaCompras" :key="iterador" :class="{ riscado: iterador.concluido }">
+      <td>
+        <input type="checkbox" v-model="iterador.concluido" />
+      </td>
+      <td>{{ iterador.descricao }}</td>
+      <td>
+        <input type="number" v-model="iterador.valor" placeholder="R$" :disabled="iterador.concluido"/>
+      </td>
+    </tr>
+  </table>
+  <app-btn @click="somarCompra()">Somar Compra</app-btn>
+  <p>{{valorCompra}}</p>
 </template>
 
-<script>
-export default{
-  data: () =>({
-    itemCompra:"",
-    itensCompras: {}
-  })
+<script setup>
+import AppBtn from "./components/AppBtn.vue";
+import { reactive, ref } from "vue";
+const titulo = ref("Lista de Compras");
+const Listas = reactive([]);
+const listaCompras = reactive([]);
+const item = reactive({
+  concluido: false,
+  descricao: "",
+  valor: Number,
+});
+let valorCompra = 0
+
+function addItem() {
+  listaCompras.push(Object.assign({}, item));
 }
+function somarCompra(){
+  for (let index = 0; index < this.listaCompras.length; index++) {
+    valorCompra += this.listaCompras[index].valor;
+    
+  }
+  return valorCompra
+}
+
 </script>
 
 <style>
-
+.riscado {
+  text-decoration: line-through;
+}
 </style>
